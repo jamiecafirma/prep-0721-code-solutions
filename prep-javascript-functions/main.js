def6eef1fe5517ae2d7ae527f48b91fa3984dad4
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-10 21:48:01 -0700

    Defined multiplyAndDivideBy5 function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -26,3 +26,11 @@ function addAndMultiplyBy5(num1, num2) {
 
 const addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
 console.log('addAndMultiplyBy5 Exercise:', addAndMultiplyBy5Result);
+
+function multiplyAndDivideBy5(num1, num2) {
+  const product = num1 * num2;
+  return product / 5;
+}
+
+const multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
+console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
