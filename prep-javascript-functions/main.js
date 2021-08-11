b38e831549dbecdad1cc8075b286dd736b1468ab
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-10 21:52:37 -0700

    Defined subtractTwoNumbers function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -34,3 +34,10 @@ function multiplyAndDivideBy5(num1, num2) {
 
 const multiplyAndDivideBy5Result = multiplyAndDivideBy5(35, 10);
 console.log('multiplyAndDivideBy5 Exercise:', multiplyAndDivideBy5Result);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+const subtractTwoNumbersResult = subtractTwoNumbers(22, 7);
+console.log('subtractTwoNumbers Exercise: ', subtractTwoNumbersResult);
