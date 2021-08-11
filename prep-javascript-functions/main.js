bbcfa6c2c1963e5e512b04652a73de572b28a285
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-10 21:43:19 -0700

    Defined addAndMultiplyBy5 function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -18,3 +18,11 @@ function getGreeting(name) {
 
 const getGreetingResult = getGreeting('World');
 console.log('getGreeting Exercise:', getGreetingResult);
+
+function addAndMultiplyBy5(num1, num2) {
+  const sum = num1 + num2;
+  return sum * 5;
+}
+
+const addAndMultiplyBy5Result = addAndMultiplyBy5(10, 5);
+console.log('addAndMultiplyBy5 Exercise:', addAndMultiplyBy5Result);
