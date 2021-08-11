97a86fefc7bc2c7a74f97c08d175f0780d1715ae
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-10 21:28:29 -0700

    Defined convertHoursToMinutes function.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+const convertHoursToMinutesResult = convertHoursToMinutes(2);
+console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
