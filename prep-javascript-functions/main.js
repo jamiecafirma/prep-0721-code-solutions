3df64048012b4dcd57a2b3cb490d5061a8299f57
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-10 21:36:07 -0700

    Defined getGreeting function.

--- prep-javascript-functions/main.js
+++ prep-javascript-functions/main.js
@@ -11,3 +11,10 @@ function convertHoursToMinutes(hours) {
 
 const convertHoursToMinutesResult = convertHoursToMinutes(2);
 console.log('convertHoursToMinutes Exercise:', convertHoursToMinutesResult);
+
+function getGreeting(name) {
+  return 'Hello ' + name + '!';
+}
+
+const getGreetingResult = getGreeting('World');
+console.log('getGreeting Exercise:', getGreetingResult);
