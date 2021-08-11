698d4c2677e537cfc1684334793cb5824e605396
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-08-11 00:20:12 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+// for (let i = 0; i < 10; i++) {
+//   console.log('for loop #1: ', i);
+// }
+
+// let x = 0;
+// for (let i = 0; i < 10; i++) {
+//   console.log('for loop #2: ', x);
+//   x += 2;
+// }
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion: ' + i + '!');
+}
